a96ece9a527dc0603d032ee8640f87e7183fc5b3
del0ct <del0ct@bochkovdv.ru>
2026-07-13 05:13:14 +0300

    Добавьте файлы проекта.

---
+++ MauiApp1/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿//using Microsoft.UI.Xaml.Controls;
+//using Microsoft.UI.Xaml.Input;
+
+namespace MauiApp1
+{
+    public partial class AboutPage : ContentPage
+    {
+        
+        public AboutPage()
+        {
+           InitializeComponent();
+
+        }
+    }
+}
---
+++ MauiApp1/AppShellAndroid.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp1
+{
+    public partial class AppShellAndroid : Shell
+    {
+        public AppShellAndroid()
+        {
+            InitializeComponent();
+        }
+    }
+}
